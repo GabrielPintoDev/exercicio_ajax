ae5ecd85e01486f483a41b5848c213e88048bd6a
GabrielPintoDev <gabrielandrade.colorado@gmail.com>
2025-07-16 19:22:45 -0300

    corrigindo erros

--- main.js
+++ main.js
@@ -9,15 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     fetch("https://api.github.com/users/GabrielPintoDev")
     .then(function(resultado) {
+        if (!resultado.ok) {
+            throw new Error("Erro na resposta da API: " + resultado.status);
+        }
         return resultado.json();
     })
     .then(function(json) {
         name.innerText = json.name;
-        username.innerText = json.username;
+        username.innerText = json.login;
         avatar.src = json.avatar_url;
         repositories.innerText = json.public_repos;
         followers.innerText = json.followers;
         following.innerText = json.following;
         link.href = json.html_url;
+    })
+    .catch(function(erro) {
+        console.error("Ocorreu um erro: ", erro);
+        alert("Erro ao buscar usuário!");
+        name.innerText = "Erro ao carregar";
     })
 })
